56d0239498b6eeb64ac9c1f6bd425c0eb9f796f3
Daniyar410 <ramziya.shakirova@gmail.com>
2024-07-30 16:47:36 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+// for (let i = 0; i < 10; i++) {
+//   console.log('hello');
+// }
+
+// let i = 0;
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 0;
+// do {
+//   i++;
+//   console.log(i);
+// } while (i > 10);
+
+// function sayHello(name) {
+//   console.log('hello ' + name);
+// }
+// sayHello('abbos')
+
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+
+// sum(50, 10)
+
+// sum(1, 2)
+
+// sum(10, 1)
+
+// sum('abbos', 'daniyar0')
+
+// sum('b', 10 )
+
+// const me = function (name, surname, age) {
+//   console.log('привет меня зовут ' + name  + ' ' + surname + ', мне ' + age);
+// };
+const me = function (name, surname, age) {
+  console.log(`привет меня зовут ${name} ${surname}, мне ${age} лет`);
+};
+
+me('daniyar', 'shakiro', 16);
